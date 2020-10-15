fa3852370bb40f3dff8597837aed327b53b793c9
inqbs <incubass@outlook.com>
2020-10-15 17:27:19 +0900

    test for vue-tutorial

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,16 @@
+<template>
+	<article :style="{fontSize: postFontSize + 'rem'}">
+		<h3>{{post.title}}</h3>
+		<button @click="$emit('bigger-text')">Size+</button>
+		<section class="content" v-html="post.content"></section>
+	</article>
+</template>
+
+<script>
+ /* eslint-disable */ 
+export default {
+	name: 'BlogPost',
+	props: ['post'],
+}
+</script>
+
---
+++ src/views/Blog.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="blog">
+    <h1>This is Blog Page</h1>
+    <BlogPost v-for="post in posts" 
+              :key="post.id" 
+              :post="post"
+              v-on:bigger-text="postFontSize += 0.1">
+    </BlogPost>
+  </div>
+</template>
+
+<script>
+import BlogPost from '@/components/BlogPost.vue'
+
+export default {
+  name: 'Blog',
+  components: {BlogPost},
+  data(){
+    return {
+      posts: [
+        { id: 1, title: 'My journey with Vue' },
+        { id: 2, title: 'Blogging with Vue' },
+        { id: 3, title: 'Why Vue is so fun' }
+      ],
+      postFontSize: 1
+    }
+  }
+}
+</script>
